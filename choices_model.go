d348bd642736ec097bcdf86fb9b1d8b425ad6192
ahmedsatti101 <ahmedysatti@gmail.com>
2025-06-26 22:32:42 +0100

    Unread mail now displays in a table structure of email id, subject and sender. Emails can also be selected to perform actions like reading an email.

---
+++ choices_model.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"strings"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+var choices = []string{"Check unread mail", "Send email", "Check spam", "Draft mail", "Search mail", "Read mail"}
+
+type choicesModel struct {
+	cursor int
+	choice string
+}
+
+func (m choicesModel) Init() tea.Cmd {
+	return nil
+}
+
+func (m choicesModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "up", "k":
+			m.cursor--
+			if m.cursor < 0 {
+				m.cursor = len(choices) - 1
+			}
+		case "down", "j":
+			m.cursor++
+			if m.cursor >= len(choices) {
+				m.cursor = 0
+			}
+		case "enter", " ":
+			m.choice = choices[m.cursor]
+			return m, tea.Quit
+		}
+	}
+	return m, nil
+}
+
+func (m choicesModel) View() string {
+	s := strings.Builder{}
+	s.WriteString("What do you need to do today?\n\n")
+
+	for i := range choices {
+		if m.cursor == i {
+			s.WriteString("(•) ")
+		} else {
+			s.WriteString("( ) ")
+		}
+		s.WriteString(choices[i])
+		s.WriteString("\n")
+	}
+	s.WriteString("\n(Press q or Ctrl+c to quit)\n")
+
+	return s.String()
+}
--- main.go
+++ main.go
@@ -3,69 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 const user = "me"
 
-var choices = []string{"Check unread mail", "Send email", "Check spam", "Draft mail", "Search mail"}
-
-type model struct {
-	cursor int
-	choice string
-}
-
-func (m model) Init() tea.Cmd {
-	return nil
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c", "q":
-			return m, tea.Quit
-		case "up", "k":
-			m.cursor--
-			if m.cursor < 0 {
-				m.cursor = len(choices) - 1
-			}
-		case "down", "j":
-			m.cursor++
-			if m.cursor >= len(choices) {
-				m.cursor = 0
-			}
-		case "enter", " ":
-			m.choice = choices[m.cursor]
-			return m, tea.Quit
-		}
-	}
-
-	return m, nil
-}
-
-func (m model) View() string {
-	s := strings.Builder{}
-	s.WriteString("What do you need to do today?\n\n")
-
-	for i := range choices {
-		if m.cursor == i {
-			s.WriteString("(•) ")
-		} else {
-			s.WriteString("( ) ")
-		}
-		s.WriteString(choices[i])
-		s.WriteString("\n")
-	}
-	s.WriteString("\n(Press q to quit)\n")
-
-	return s.String()
-}
-
 func main() {
-	p := tea.NewProgram(model{})
+	p := tea.NewProgram(choicesModel{}, tea.WithAltScreen())
 
 	m, err := p.Run()
 	if err != nil {
@@ -73,19 +18,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, ok := m.(model)
+	_, ok := m.(choicesModel)
 
-	if ok && m.(model).choice != "" {
-		switch m.(model).choice {
+	if ok && m.(choicesModel).choice != "" {
+		switch m.(choicesModel).choice {
 		case "Check unread mail":
 			unreadMail(user)
-		case "Send email":
-			fmt.Println("Not available yet")
-		case "Check spam":
-			fmt.Println("Not available yet")
-		case "Draft mail":
-			fmt.Println("Not available yet")
-		case "Search mail":
+		default:
 			fmt.Println("Not available yet")
 		}
 	}
